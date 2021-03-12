a4c3ade9a5c9494fb0c567aad493e887bf70b779
anderson <tandersonf@hotmail.com>
2021-03-12 12:52:45 -0300

    funcoes adicionadas com botoes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+const Container = styled.div`
+height:100vh;
+/* display:flex */
+`
+const ContainerButtons = styled.div`
+position:fixed;
+bottom: 0;
+width:100%;
+/* left: 30% */
+
+
+`
+
+
+class App extends React.Component {
+  state = {
+    posts: [
+      {
+        remetente: "",
+        conteudo: ""
+      }
+    ],
+    novoRemetente: "",
+    novoConteudo: ""
+  }
+
+  onChangeInputNovoRemente = event => {
+    this.setState({ novoRemetente: event.target.value })
+  }
+
+  onChangeInputNovoConteudo = event => {
+    this.setState({ novoConteudo: event.target.value })
+  }
+
+  novaMensagemPostada = () => {
+    const novaMensagem = {
+      remetente: this.state.novoRemetente,
+      novoConteudo: this.state.novoConteudo
+    }
+    const mensagensAtualizadas = [...this.state.posts, novaMensagem]
+    this.setState({ posts: mensagensAtualizadas })
+    this.setState({ novoRemetente: "", novoConteudo: "" })
+  }
+
+
+
+  render() {
+
+    const mensagemsComponentes = this.state.posts.map((post) => {
+      return (
+          <div>
+          <span><i>{post.remetente}</i>:   </span>
+          <span>{post.novoConteudo}</span>
+          </div>
+        )
+    })
+
+    return (
+      <div className="App">
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
-}
+        </header> */}
 
+<Container>
+{mensagemsComponentes}
+<ContainerButtons>
+<input value={this.state.novoRemetente} onChange={this.onChangeInputNovoRemente}/>
+<input value={this.state.novoConteudo} onChange={this.onChangeInputNovoConteudo}/>
+<button onClick={this.novaMensagemPostada}>Adicionar comentário</button>
+</ContainerButtons>
+</Container>
+        
+      </div>
+    );
+  }
+}
 export default App;
