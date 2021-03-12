9584ccf012960c8522ea79fee7a11dafc45a2359
anderson <tandersonf@hotmail.com>
2021-03-12 16:38:07 -0300

    Ainda sem componentização, criado os arquivos.js

---
+++ src/components/ContainerButtons.js
@@ -0,0 +1,24 @@
+// import React from 'react';
+// import styled from 'styled-components'
+
+// const ContainerButtons = styled.div`
+// position:fixed;
+// bottom: 0;
+// width:100%;
+// /* left: 30% */
+
+
+// `
+
+// export function ContainerButtons (props){
+//     render() {
+//         return( 
+//             <ContainerButtons>
+// <input placeholder="Remetente" value={props.remetente} onChange={this.onChangeInputNovoRemente} onKeyDown={this.onKeyUpPostarMensagem}/>
+// <input placeholder="Mensagem" value={props.mensagem} onChange={this.onChangeInputNovoConteudo} onKeyDown={this.onKeyUpPostarMensagem}/>
+// <button onClick={this.novaMensagemPostada}>Adicionar comentário</button>
+// </ContainerButtons>
+//         )
+//     }
+// }
+// export default ContainerButtons
